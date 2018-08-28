cfd4253b3950ed2eb25f0d5cef817d32b7e1bfbe
joylty94 <wowmen1757@gmail.com>
2018-08-28 21:31:07 +0900

    action 수정

--- actions/index.js
+++ actions/index.js
@@ -1,15 +1,15 @@
 import { ADD_NEW_TASK, TOGGLE_ONE_TASK } from './actionTypes';
 let newTaskId = 0;
 
-export const addNEWTASK = (inputTaskName) => {
+export const addNewTask = (inputTaskName) => {
   return{
     type: ADD_NEW_TASK,
-    taskId: newTaskId,
+    taskId: newTaskId++,
     taskName: inputTaskName,
   }
 }
 
-export const toggleTask = (teskId) => {
+export const toggleTask = (taskId) => {
   return{
     type: TOGGLE_ONE_TASK,
     taskId: taskId
